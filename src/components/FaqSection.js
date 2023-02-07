22e64e7431b1d332804eb1fcaeea8c9118eb7aa1
Amir Samoha <amir.samoha42@gmail.com>
2023-02-07 11:36:04 +0200

    sections components

---
+++ src/components/FaqSection.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+function FaqSection() {
+  return (
+    <div className="faq">
+        <h2>Any Questions <span>FAQ</span></h2>
+        <div className="question">
+            <h4>How Do I Start?</h4>
+            {/* לשנות למשהו שמתאים אליי  */}
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>Daily Schedule?</h4>
+            {/* לשנות למשהו שמתאים אליי  */}
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>Diferrent Payment Methods?</h4>
+            {/* לשנות למשהו שמתאים אליי  */}
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>What Product do you offer?</h4>
+            {/* לשנות למשהו שמתאים אליי  */}
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default FaqSection;
