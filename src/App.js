fd512e8bc4426d7d35c54c79d6b8200836ce6fa9
Amir Samoha <amir.samoha42@gmail.com>
2023-02-13 18:49:40 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from "react";
 // import global style
 import GlobalStyle from "./components/GlobalStyle";
 // import pages
-import AboutUs from './pages/AboutUs'
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+//router switcher between pages
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Routes>
+        {/* הרגע שהנתיב ביו אר אל יהיה עם סלאש והשם שלו ידע לעבור לדף הזה */}
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/aboutus" exact element={<AboutUs />} />
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
